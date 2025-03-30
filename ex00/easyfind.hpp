43ecc05e1161881aea7a4579ae0414f819e0b1ae
Rino Naito <rnaito@c5r3s1.42tokyo.jp>
2025-03-30 13:34:49 +0900

    MOD: make ex00 compilable with Linux

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include<iostream>
 #include<exception>
+#include<algorithm>
 
-// this function has to find the first occurrence
-// of the second parameter in the first parameter
 template <typename T>
 typename T::iterator easyfind(T &container, int target){
-	typename T::iterator it = find(container.begin(), container.end(), target);
+	typename T::iterator it = std::find(container.begin(), container.end(), target);
 	if (it == container.end())
 		throw std::runtime_error("[ERROR] Target Not Found.");
 	return it;
